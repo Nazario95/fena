244b1af9b574b599e0d50792df3200ea62b2177c
Nazario Muanbama <nazariomuanbama@gmail.com>
2024-07-04 04:55:38 +0100

    v0.5_04.07.2024_insert-personal-secction

---
+++ assets/js/custom.js
@@ -0,0 +1,25 @@
+(function() {
+	'use strict';
+
+	var tinyslider = function() {
+		var el = document.querySelectorAll('.testimonial-slider');
+
+		if (el.length > 0) {
+			var slider = tns({
+				container: '.testimonial-slider',
+				items: 1,
+				axis: "horizontal",
+				controlsContainer: "#testimonial-nav",
+				swipeAngle: false,
+				speed: 700,
+				nav: true,
+				controls: true,
+				autoplay: false,
+				autoplayHoverPause: true,
+				autoplayTimeout: 10000,
+				autoplayButtonOutput: false
+			});
+		}
+	};
+	tinyslider();
+})()
